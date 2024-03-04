f1f1e22155eaa01d5c97be8077a6f75cfbb60a0b
Tmplinkz <139988794+Tmplinkz@users.noreply.github.com>
2024-03-04 15:19:33 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -95,7 +95,7 @@ async def start_command(client: Client, message: Message):
 
         for snt_msg in snt_msgs:
             try:
-                await snt_msg.delete()
+                await snt_msg.()
             except:
                 pass
         return
