c8f0eca0ccaf7caf00c65c5e2bfbfd41988a33c8
Tmplinkz <139988794+Tmplinkz@users.noreply.github.com>
2024-03-07 07:42:58 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -17,7 +17,7 @@ from database.database import add_user, del_user, full_userbase, present_user
 
 """add time im seconds for waitingwaiting before delete 
 1min=60, 2min=60×2=120, 5min=60×5=300"""
-SECONDS = int(os.getenv("SECONDS", "3600")) 
+SECONDS = int(os.getenv("SECONDS", "900")) 
 
 
 @Bot.on_message(filters.command('start') & filters.private & subscribed)
@@ -90,7 +90,7 @@ async def start_command(client: Client, message: Message):
             except:
                 pass
 
-        await message.reply_text("‼️Fɪʟᴇ ᴡɪʟʟ ᴀᴜᴛᴏ ᴅᴇʟᴇᴛᴇ ɪɴ 1 Hour😱\n_Fᴏʀᴡᴀʀᴅ ɪᴛ ᴛᴏ sᴀᴠᴇᴅ ᴍᴇssᴀɢᴇs ᴏʀ ᴀɴʏᴡʜᴇʀᴇ ʙᴇғᴏʀᴇ ᴅᴏᴡɴʟᴏᴀᴅɪɴɢ.")
+        await message.reply_text("‼️Fɪʟᴇ ᴡɪʟʟ ᴀᴜᴛᴏ ᴅᴇʟᴇᴛᴇ ɪɴ 15 minutes 😱\n_Fᴏʀᴡᴀʀᴅ ɪᴛ ᴛᴏ sᴀᴠᴇᴅ ᴍᴇssᴀɢᴇs ᴏʀ ᴀɴʏᴡʜᴇʀᴇ ʙᴇғᴏʀᴇ ᴅᴏᴡɴʟᴏᴀᴅɪɴɢ.")
         await asyncio.sleep(SECONDS)
 
         for snt_msg in snt_msgs:
